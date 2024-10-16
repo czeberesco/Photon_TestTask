51e97f71a27213ef21341cbd9540c14ff7d52565
PawelCzebreszuk <czebreszuk.pawel@gmail.com>
2024-10-16 22:01:16 +0200

    Move app specific assets under common directory, add project context

---
+++ Photon_TestTask/Assets/App/Scripts/Core/Installers/ProjectInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Fusion;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Installers
+{
+	public class ProjectInstaller : MonoInstaller
+	{
+		#region SerializeFields
+
+		[SerializeField] private NetworkRunner m_networkRunner;
+
+		#endregion
+
+		#region PublicMethods
+
+		public override void InstallBindings()
+		{
+			Container.Bind<NetworkRunner>().FromInstance(m_networkRunner).AsSingle();
+		}
+
+		#endregion
+	}
+}
