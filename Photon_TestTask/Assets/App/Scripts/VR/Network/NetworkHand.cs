7e60e61fd8d5ab81dd820f0be4e2bb6016896335
PawelCzebreszuk <czebreszuk.pawel@gmail.com>
2024-10-25 12:12:00 +0200

    Update hand view for proxies through change detector

--- Photon_TestTask/Assets/App/Scripts/VR/Network/NetworkHand.cs
+++ Photon_TestTask/Assets/App/Scripts/VR/Network/NetworkHand.cs
@@ -1,16 +1,31 @@
-﻿using Fusion;
+﻿using Core.Interfaces;
+using Fusion;
 using UnityEngine;
 using Utils;
 using VR.Views;
+using Zenject;
 
 namespace VR.Network
 {
 	[DefaultExecutionOrder(R.ExecutionOrder.NETWORK_HAND)]
 	public class NetworkHand : NetworkBehaviour
 	{
+		#region Properties
+
+		[Networked] private ControllerInput ControllerInput { get; set; }
+
+		#endregion
+
 		#region SerializeFields
 
 		[SerializeField] private HandView m_handView;
+
+		#endregion
+
+		#region PrivateFields
+
+		[Inject] private INetworkRunnerProvider m_networkRunnerProvider;
+		private ChangeDetector m_changeDetector;
 
 		#endregion
 
@@ -18,7 +33,40 @@ namespace VR.Network
 
 		public void SetControllerInput(ControllerInput controllerInput)
 		{
+			if (Object.HasInputAuthority && m_networkRunnerProvider.Runner.IsServer)
+			{
+				ControllerInput = controllerInput;
+			}
+
 			m_handView.UpdateHandViewInput(controllerInput);
+		}
+
+		public override void Spawned()
+		{
+			base.Spawned();
+			m_changeDetector = GetChangeDetector(ChangeDetector.Source.SnapshotFrom);
+		}
+
+		public override void Render()
+		{
+			base.Render();
+
+			UpdateProxiesControllerInput();
+		}
+
+		#endregion
+
+		#region PrivateMethods
+
+		private void UpdateProxiesControllerInput()
+		{
+			foreach (string change in m_changeDetector.DetectChanges(this))
+			{
+				if (change == nameof(ControllerInput))
+				{
+					m_handView.UpdateHandViewInput(ControllerInput);
+				}
+			}
 		}
 
 		#endregion
